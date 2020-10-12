4cb3daaa11fef9c8038f76415bdb5beba08f5960
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-10-12 16:17:14 -0400

    Clean up comments

--- snow/networking/router/service_queue_test.go
+++ snow/networking/router/service_queue_test.go
@@ -1,5 +1,5 @@
-// // (c) 2019-2020, Ava Labs, Inc. All rights reserved.
-// // See the file LICENSE for licensing terms.
+// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
 
 package router
 
@@ -78,7 +78,6 @@ func TestMultiLevelQueueSendsMessages(t *testing.T) {
 		}
 	}
 
-	// Ensure that the 6th message was never added to the queue
 	select {
 	case <-semaChan:
 		t.Fatal("Semaphore channel should have been empty after reading all messages from the queue")
